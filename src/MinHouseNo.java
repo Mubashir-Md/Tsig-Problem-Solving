b5614b144d85f88441a444b32eeeb8a58ffcd51f
Mubashir <mubashir.ma061@gmail.com>
2022-11-08 18:13:24 +0530

    First day

---
+++ src/MinHouseNo.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class MinHouseNo {
+    public static void main(String args[]) throws IOException {
+        //PrepBuddy lives in a colony, where N houses are built in a single row numbered from 0 to N − 1 .
+        //The first house has a house number 0 , the second house has a house number 1 and so on, every house pays some
+        // rent at the end of the month. Help PrepBuddy in finding out the house number of the house paying the minimum rent. Note: All house rents are unique.
+        // Mubashir
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while(t!=0){
+
+            int size = in.nextInt();
+            int[] arr = new int[size];
+
+
+            for(int i=0;i<size;i++)
+                arr[i] = in.nextInt();
+            int min = arr[0];
+            int minIndex = 0;
+
+            for(int i=0;i<size;i++){
+                if(min>arr[i]){
+                    min = arr[i];
+                    minIndex = i;
+                }
+            }
+            System.out.println(minIndex);
+            t--;
+        }
+
+    }
+}
---
+++ src/ScalarMultiply.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class ScalarMultiply {
+    public static void main(String args[]) throws IOException {
+        //You are given a M ∗ N matrix, and a variable K , print the resultant matrix after scalar multiplication.
+        // Mubashir
+        Scanner in = new Scanner(System.in);
+        int m = in.nextInt();
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[][] mat = new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int x = in.nextInt();
+                mat[i][j] = x*k;
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
